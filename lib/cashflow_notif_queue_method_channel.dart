55759f9d4fc957700584258bacc8307e4dc6b215
abs3112 <absrivastav23@gmail.com>
2026-01-14 15:02:30 +0530

    DART API configured

--- lib/cashflow_notif_queue_method_channel.dart
+++ lib/cashflow_notif_queue_method_channel.dart
@@ -3,15 +3,45 @@ import 'package:flutter/services.dart';
 
 import 'cashflow_notif_queue_platform_interface.dart';
 
-/// An implementation of [CashflowNotifQueuePlatform] that uses method channels.
 class MethodChannelCashflowNotifQueue extends CashflowNotifQueuePlatform {
-  /// The method channel used to interact with the native platform.
   @visibleForTesting
-  final methodChannel = const MethodChannel('cashflow_notif_queue');
+  final MethodChannel methodChannel = const MethodChannel('cashflow_notif_queue');
 
   @override
   Future<String?> getPlatformVersion() async {
     final version = await methodChannel.invokeMethod<String>('getPlatformVersion');
     return version;
+  }
+
+  @override
+  Future<bool> openNotificationListenerSettings() async {
+    final res = await methodChannel.invokeMethod<bool>('openNotificationListenerSettings');
+    return res ?? false;
+  }
+
+  @override
+  Future<bool> isListenerConnected() async {
+    final res = await methodChannel.invokeMethod<bool>('isListenerConnected');
+    return res ?? false;
+  }
+
+  @override
+  Future<int> peekCount() async {
+    final res = await methodChannel.invokeMethod<dynamic>('peekCount');
+    if (res is int) return res;
+    if (res is num) return res.toInt();
+    return 0;
+  }
+
+  @override
+  Future<String> drain() async {
+    final res = await methodChannel.invokeMethod<String>('drain');
+    return res ?? '[]';
+  }
+
+  @override
+  Future<bool> clear() async {
+    final res = await methodChannel.invokeMethod<bool>('clear');
+    return res ?? false;
   }
 }
--- lib/cashflow_notif_queue_platform_interface.dart
+++ lib/cashflow_notif_queue_platform_interface.dart
@@ -3,27 +3,40 @@ import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 import 'cashflow_notif_queue_method_channel.dart';
 
 abstract class CashflowNotifQueuePlatform extends PlatformInterface {
-  /// Constructs a CashflowNotifQueuePlatform.
   CashflowNotifQueuePlatform() : super(token: _token);
 
   static final Object _token = Object();
 
   static CashflowNotifQueuePlatform _instance = MethodChannelCashflowNotifQueue();
 
-  /// The default instance of [CashflowNotifQueuePlatform] to use.
-  ///
-  /// Defaults to [MethodChannelCashflowNotifQueue].
   static CashflowNotifQueuePlatform get instance => _instance;
 
-  /// Platform-specific implementations should set this with their own
-  /// platform-specific class that extends [CashflowNotifQueuePlatform] when
-  /// they register themselves.
   static set instance(CashflowNotifQueuePlatform instance) {
-    PlatformInterface.verifyToken(instance, _token);
+    PlatformInterface.verify(instance, _token);
     _instance = instance;
   }
 
   Future<String?> getPlatformVersion() {
-    throw UnimplementedError('platformVersion() has not been implemented.');
+    throw UnimplementedError('getPlatformVersion() has not been implemented.');
+  }
+
+  Future<bool> openNotificationListenerSettings() {
+    throw UnimplementedError('openNotificationListenerSettings() not implemented.');
+  }
+
+  Future<bool> isListenerConnected() {
+    throw UnimplementedError('isListenerConnected() not implemented.');
+  }
+
+  Future<int> peekCount() {
+    throw UnimplementedError('peekCount() not implemented.');
+  }
+
+  Future<String> drain() {
+    throw UnimplementedError('drain() not implemented.');
+  }
+
+  Future<bool> clear() {
+    throw UnimplementedError('clear() not implemented.');
   }
 }
